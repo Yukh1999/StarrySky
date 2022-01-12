36a281d7502b115a17476616546f01ecbbe2e7e5
Yukh1999 <1104157655@qq.com>
2022-01-12 23:50:45 +0800

    猫态 wigner function

--- cat qubit/Cat qubit ctrl pulse/CatQubit_WignerFunction.py
+++ cat qubit/Cat qubit ctrl pulse/CatQubit_WignerFunction.py
@@ -1,4 +1,41 @@
-from qutip import destroy
+import numpy as np
+from qutip import destroy, coherent, wigner
+from matplotlib import pyplot as plt
+
+# 维度
+N = 30
+
+# 相干态
+alp = 2
+alpha = coherent(N=N, alpha=alp)
+alpha_m = coherent(N=N, alpha=-alp)
+alpha_i = coherent(N=N, alpha=1j * alp)
+alpha_i_m = coherent(N=N, alpha=-1j * alp)
+
+# 逻辑比特编码态
+logic_0 = np.sqrt(2) * (alpha + alpha_m)
+logic_1 = np.sqrt(2) * (alpha_i + alpha_i_m)
+
+# 编码态的密度矩阵
+rho_0 = logic_0 * logic_0.dag()
+rho_1 = logic_1 * logic_1.dag()
 
 
-destroy(5, 2)
+# wigner 函数既可以输入量子态，也可以输入密度矩阵
+# 0 态的 wigner 函数
+vec = np.linspace(-5, 5, 202)  # 计算范围
+W1 = wigner(psi=logic_0, xvec=vec, yvec=vec)
+
+# 1 态的 wigner 函数
+W2 = wigner(psi=logic_1, xvec=vec, yvec=vec)
+
+
+# 绘制 wigner 函数图像
+plt.figure(figsize=(6, 6))
+plt.contourf(vec, vec, W1, 100, cmap=plt.get_cmap('RdBu'))
+
+plt.figure(figsize=(6, 6))
+plt.contourf(vec, vec, W2, 100, cmap=plt.get_cmap('RdBu'))
+plt.show()
+
+

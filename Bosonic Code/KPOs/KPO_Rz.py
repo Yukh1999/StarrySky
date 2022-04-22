e53856fa4c542399029870dbf0b5c4a5ef648f7e
Yukh1999 <1104157655@qq.com>
2022-04-23 00:07:17 +0800

    KPO_Rz_gate

--- Bosonic Code/KPOs/KPO_Rz.py
+++ Bosonic Code/KPOs/KPO_Rz.py
@@ -10,12 +10,11 @@ from scipy import integrate
 import krotov
 from matplotlib import pyplot as plt
 
-
 # 定义常数
 N = 20  # 维度
-delta = 0  # 失谐
+delta = 10  # 失谐
 K = 1  # 非线性强度
-P = 4 * K  # 双光子驱动强度
+p0 = 4 * K  # 双光子驱动强度
 
 # 定义算符
 a = destroy(N)
@@ -23,16 +22,24 @@ a_dag = a.dag()
 I = qeye(N)
 
 # 定义逻辑比特态
-alpha = np.sqrt(P / K)
+alpha = np.sqrt(p0 / K)
 logical_0 = coherent(N=20, alpha=alpha)
 logical_1 = coherent(N=20, alpha=-alpha)
+
+
+def P(_t, _phi, _Tg):
+    """
+    Two-photon driving amplitude
+    """
+
+    return p0
 
 
 def P_d(_t, _phi, _Tg):
     """
     Driving pulse
     """
-    return np.pi * _phi * np.sin(np.pi * _t / _Tg) / (8 * _Tg * np.sqrt(P / K))
+    return np.pi * _phi * np.sin(np.pi * _t / _Tg) / (8 * _Tg * np.sqrt(p0 / K))
 
 
 def gaussian(_t, _phi, _Tg):
@@ -43,6 +50,13 @@ def gaussian(_t, _phi, _Tg):
     return (np.pi / 2) / (sigma * np.sqrt(2 * np.pi)) * np.exp(-0.5 * (_t / sigma) ** 2)
 
 
+def tempPulse(_t, _phi, _Tg):
+    """
+    Temp pulse
+    """
+    return np.sqrt(P(_t, _phi, _Tg) / K) * krotov.shapes.blackman(_t, 0, _Tg)
+
+
 def blackman_pulse(_t, _phi, _Tg):
     """
     black man pulse
@@ -50,11 +64,13 @@ def blackman_pulse(_t, _phi, _Tg):
     sigma = _Tg / 6
 
     # 计算振幅
-    blackman_integ = integrate.quad(krotov.shapes.blackman, 0, _Tg, args=(0, _Tg))[0]
+    # blackman_integ = integrate.quad(krotov.shapes.blackman, 0, _Tg, args=(0, _Tg))[0]
+    integ = integrate.quad(tempPulse, 0, _Tg, args=(_phi, _Tg))[0]
 
-    factor = _phi / (4 * np.sqrt(P / K))
+    amp = _phi / (integ * 4)
+    # factor = _phi / (4 * np.sqrt(p0 / K))
 
-    amp = factor / blackman_integ
+    # amp = factor / blackman_integ
 
     return amp * krotov.shapes.blackman(_t, t_start=0, t_stop=_Tg)
 
@@ -77,7 +93,7 @@ def ham(_t, args):
     """
     Hamiltonian
     """
-    ham_1 = delta * a_dag * a + K / 2 * (a_dag ** 2) * (a ** 2) - P / 2 * (a_dag ** 2 + a ** 2)
+    ham_1 = delta * a_dag * a + K / 2 * (a_dag ** 2) * (a ** 2) - p0 / 2 * (a_dag ** 2 + a ** 2)
     ham_d = P_d(_t, *args) * (a + a_dag)
 
     return ham_1 + ham_d
@@ -87,7 +103,8 @@ def ham_black(_t, args):
     """
     Hamiltonian with black man wave
     """
-    ham_1 = delta * a_dag * a + K / 2 * (a_dag ** 2) * (a ** 2) - P / 2 * (a_dag ** 2 + a ** 2)
+    # ham_1 = delta * a_dag * a + K / 2 * (a_dag ** 2) * (a ** 2) - p0 / 2 * (a_dag ** 2 + a ** 2)
+    ham_1 = delta * a_dag * a + K / 2 * (a_dag ** 2) * (a ** 2) - P(_t, *args) / 2 * (a_dag ** 2 + a ** 2)
     ham_blackman = blackman_pulse(_t, *args) * (a + a_dag)
 
     return ham_1 + ham_blackman
@@ -98,7 +115,7 @@ Tg = 2 / K
 
 # 期望的 Rz 门旋转角度
 phi = np.pi
-phi_ls = np.linspace(-np.pi, np.pi, 21)
+phi_ls = np.linspace(-np.pi, np.pi, 11)
 
 # 时间列表
 tlist = np.linspace(0, Tg, 100)
@@ -126,8 +143,9 @@ def solve_fidelity(_ham, _initial_state, tls, args):
     return np.abs(np.array(final_state.dag() * ideal_state)[0][0]) ** 2
 
 
-# plot_wave(blackman_pulse, tlist, args=(phi, Tg))
-# plot_wave(P_d, tlist, args=(phi, Tg))
+plot_wave(blackman_pulse, tlist, args=(phi, Tg))
+plot_wave(P_d, tlist, args=(phi, Tg))
+
 
 def get_fidelity_ham(_phi):
     return solve_fidelity(ham, initial_state, tlist, args=(_phi, Tg))

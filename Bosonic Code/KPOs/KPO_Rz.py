c1a45ccc567f9cb489ee30915c93681176d8d014
Yukh1999 <1104157655@qq.com>
2022-04-22 18:50:34 +0800

    KPO_Rz_gate

--- Bosonic Code/KPOs/KPO_Rz.py
+++ Bosonic Code/KPOs/KPO_Rz.py
@@ -6,6 +6,8 @@ Rz(φ) gate simulation
 
 from qutip import coherent, destroy, qeye, mesolve
 import numpy as np
+from scipy import integrate
+import krotov
 from matplotlib import pyplot as plt
 
 
@@ -21,7 +23,7 @@ a_dag = a.dag()
 I = qeye(N)
 
 # 定义逻辑比特态
-alpha = np.sqrt(P/K)
+alpha = np.sqrt(P / K)
 logical_0 = coherent(N=20, alpha=alpha)
 logical_1 = coherent(N=20, alpha=-alpha)
 
@@ -30,33 +32,133 @@ def P_d(_t, _phi, _Tg):
     """
     Driving pulse
     """
+    return np.pi * _phi * np.sin(np.pi * _t / _Tg) / (8 * _Tg * np.sqrt(P / K))
 
-    return np.pi * _phi * np.sin(np.pi * _t / _Tg) / (8 * _Tg * np.sqrt(P/K))
+
+def gaussian(_t, _phi, _Tg):
+    """
+    Gaussian wave
+    """
+    sigma = _Tg / 6
+    return (np.pi / 2) / (sigma * np.sqrt(2 * np.pi)) * np.exp(-0.5 * (_t / sigma) ** 2)
 
 
-def ham(_t, *args):
+def blackman_pulse(_t, _phi, _Tg):
+    """
+    black man pulse
+    """
+    sigma = _Tg / 6
+
+    # 计算振幅
+    blackman_integ = integrate.quad(krotov.shapes.blackman, 0, _Tg, args=(0, _Tg))[0]
+
+    factor = _phi / (4 * np.sqrt(P / K))
+
+    amp = factor / blackman_integ
+
+    return amp * krotov.shapes.blackman(_t, t_start=0, t_stop=_Tg)
+
+
+def plot_wave(wave, tls, args):
+    """
+    plot wave
+    """
+    plt.figure()
+
+    wave_sequence = []
+    for _t in tls:
+        wave_sequence.append(wave(_t, *args))
+
+    plt.plot(tls, wave_sequence)
+    plt.show()
+
+
+def ham(_t, args):
     """
     Hamiltonian
     """
-
-    ham_1 = delta * a_dag * a + K/2 * (a_dag ** 2) * (a ** 2) - P/2 * (a_dag ** 2 + a ** 2)
+    ham_1 = delta * a_dag * a + K / 2 * (a_dag ** 2) * (a ** 2) - P / 2 * (a_dag ** 2 + a ** 2)
     ham_d = P_d(_t, *args) * (a + a_dag)
 
     return ham_1 + ham_d
 
 
+def ham_black(_t, args):
+    """
+    Hamiltonian with black man wave
+    """
+    ham_1 = delta * a_dag * a + K / 2 * (a_dag ** 2) * (a ** 2) - P / 2 * (a_dag ** 2 + a ** 2)
+    ham_blackman = blackman_pulse(_t, *args) * (a + a_dag)
+
+    return ham_1 + ham_blackman
+
+
 # 脉冲总时间
-Tg = 2/K
-phi = np.pi / 4
+Tg = 2 / K
+
+# 期望的 Rz 门旋转角度
+phi = np.pi
+phi_ls = np.linspace(-np.pi, np.pi, 21)
+
+# 时间列表
 tlist = np.linspace(0, Tg, 100)
 
-initial_state = (logical_0 + logical_1) / np.sqrt(2)
+# 初态
+initial_state = (logical_0 + logical_1).unit()
 
-final_state = mesolve(ham, initial_state, tlist=tlist, args=[phi, Tg])
-
-phase_factor = 1j * phi / 2
-ideal_state = (logical_0 * np.exp(-phase_factor) + logical_1 * np.exp(phase_factor)) / np.sqrt(2)
-
-fidelity = 1
+# 进行含时演化
+fidelity_ls = []
+fidelity_BLK_ls = []
 
 
+def solve_fidelity(_ham, _initial_state, tls, args):
+    """
+    Solving fidelity
+    """
+    res = mesolve(_ham, _initial_state, tlist=tls, args=args)
+
+    # 末态
+    final_state = res.states[-1]
+
+    phase_factor = 1j * args[0] / 2
+    ideal_state = (logical_0 * np.exp(-phase_factor) + logical_1 * np.exp(phase_factor)).unit()
+
+    return np.abs(np.array(final_state.dag() * ideal_state)[0][0]) ** 2
+
+
+# plot_wave(blackman_pulse, tlist, args=(phi, Tg))
+# plot_wave(P_d, tlist, args=(phi, Tg))
+
+def get_fidelity_ham(_phi):
+    return solve_fidelity(ham, initial_state, tlist, args=(_phi, Tg))
+
+
+def get_fidelity_ham_blk(_phi):
+    return solve_fidelity(ham_black, initial_state, tlist, args=(_phi, Tg))
+
+
+for phi in phi_ls:
+    fidelity = get_fidelity_ham(phi)
+    fidelity_ls.append(fidelity)
+    fidelity_BLK = get_fidelity_ham_blk(phi)
+    fidelity_BLK_ls.append(fidelity_BLK)
+
+fidelity_ls = np.array(fidelity_ls)
+infidelity_ls = 1 - fidelity_ls
+
+fidelity_BLK_ls = np.array(fidelity_BLK_ls)
+infidelity_BLK_ls = 1 - fidelity_BLK_ls
+
+# 绘图
+plt.figure()
+plt.plot(phi_ls, infidelity_ls, label='infidelity')
+plt.scatter(phi_ls, infidelity_ls)
+
+plt.plot(phi_ls, infidelity_BLK_ls, label='infidelity_BLK')
+plt.scatter(phi_ls, infidelity_BLK_ls)
+
+plt.xlabel(r'$\phi$')
+plt.ylabel('Infidelity')
+
+plt.legend()
+plt.show()
